7ebf4f559f65f87587d5ac2fba0e54f408a3ad4f
Diffa Azkhani <diffaazkhani1@gmail.com>
2023-05-24 19:45:16 +0700

    Menambahkan program tampil data

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ include 'koneksi.php';
     <h1 style="text-align: center">Program CRUD</h1>
     <h2 style="text-align: center">Diffa Azkhani (A12.2020.06496)</h2>
 
+    <!-- Input data mahasiswa -->
     <h3>Form Input Data</h3>
     <form action="" method="post">
         <table>
@@ -36,6 +37,36 @@ include 'koneksi.php';
             </tr>
         </table>
     </form>
+
+    <!-- Menampilkan Data -->
+    <h3>Tampil Data</h3>
+    <table border="2">
+        <tr>
+            <th>No</th>
+            <th>NIM</th>
+            <th>NAMA</th>
+            <th>IPK</th>
+            <th colspan="2">Aksi</th>
+        </tr>
+        <tr>
+            <?php
+            $no = 1;
+            $ambilData = mysqli_query($koneksi, "SELECT * FROM mahasiswa");
+            while ($tampil = mysqli_fetch_array($ambilData)) {
+                echo "
+                <tr>
+                <td>$no</td>
+                <td>$tampil[nim_mahasiswa]</td>
+                <td>$tampil[nama_mahasiswa]</td>
+                <td>$tampil[ipk_mahasiswa]</td>
+                <td><a href='?hpsdata=$tampil[nim_mahasiswa]'>Hapus</a></td>
+                <td><a href='?edtdata=$tampil[nim_mahasiswa]'>Edit</a></td>
+                </tr>";
+                $no++;
+            }
+            ?>
+        </tr>
+    </table>
 </body>
 
 </html>
